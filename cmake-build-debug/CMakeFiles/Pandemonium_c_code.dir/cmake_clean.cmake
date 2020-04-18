0a4fb7a6e5381d05fcde51563b3d3ccd124bc432
anonymousnick <60456897+anonymousnick@users.noreply.github.com>
2020-04-18 16:48:50 -0700

    test

--- cmake-build-debug/CMakeFiles/Pandemonium_c_code.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Pandemonium_c_code.dir/cmake_clean.cmake
@@ -1,8 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Pandemonium_c_code.dir/Piece.cpp.o"
-  "CMakeFiles/Pandemonium_c_code.dir/GameState.cpp.o"
+  "CMakeFiles/Pandemonium_c_code.dir/Piece.cpp.obj"
+  "CMakeFiles/Pandemonium_c_code.dir/GameState.cpp.obj"
   "Pandemonium_c_code.pdb"
-  "Pandemonium_c_code"
+  "Pandemonium_c_code.exe"
+  "Pandemonium_c_code.exe.manifest"
+  "libPandemonium_c_code.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
